31eb35c3ede1b6f68bd74a3d1f2fe3586f599448
Phan Vo <phanvo@gmail.com>
2020-06-26 21:47:05 -0700

    init files; implement basic structure for desktop, tablet layout; implement sticky navigation bar for tablet layout

---
+++ script/scripts_PVo20809.js
@@ -0,0 +1,11 @@
+// window.onscroll = function() {
+//     var header = document.getElementById("navbar");
+//     var sticky = header.offsetTop;
+//     if (window.pageYOffset > sticky) {
+//         header.classList.add("sticky");
+//     } else {
+//         header.classList.remove("sticky");
+//     }
+// };
+
+
