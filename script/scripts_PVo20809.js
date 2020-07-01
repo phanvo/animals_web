dd53256b05d75a594987830069187a2fe8b03a67
Phan Vo <phanvo@gmail.com>
2020-06-30 22:13:22 -0700

    implement form validation using js only; add fish images; add references; fix bugs

--- script/scripts_PVo20809.js
+++ script/scripts_PVo20809.js
@@ -51,17 +51,135 @@ function topFunction() {
 // }
 
 
-// format phone number nicely
-function formatPhoneNumber (form) {
-  var str = form.elements["phone"].value;
 
-  //Filter only numbers from the input
-  var cleaned = ('' + str).replace(/\D/g, '');
+function validateRequiredInput(obj){
+  // check empty input
+  if(obj.value.trim().length == 0){
+    obj.setCustomValidity("Input cannot be empty");
+    return false;
+  } else {
+    obj.setCustomValidity("");
+    return true;
+  }
+}
+
+// validate form
+function validateForm(form){
+  validateFirstName(form);
+  validateLastName(form);
+  validateEmail(form);
+  validateGender(form);
+  validatePhoneNumber(form);
+  validateEnquiry(form);
+  validateMessage(form);
+}
+
+// validate name
+function validateName(obj, typeName){
+  var isValid = validateRequiredInput(obj);
+  if(!isValid){
+    return;
+  }
+
+  // check if the email has correct format
+  var match = obj.value.match(/^[A-Za-z\s]{1,30}$/);
+
+  if(match){
+    obj.setCustomValidity("");
+  } else {
+    obj.setCustomValidity("Invalid " + typeName + ". Please check again");
+  }
+}
+
+// validate first name
+function validateFirstName(form){
+  var obj = form.elements["firstName"];
+
+  validateName(obj, "first name");
+}
+
+// validate last name
+function validateLastName(form){
+  var obj = form.elements["lastName"];
+
+  validateName(obj, "last name");
+}
+
+// validate email
+function validateEmail(form){
+  var obj = form.elements["email"];
+
+  var isValid = validateRequiredInput(obj);
+  if(!isValid){
+    return;
+  }
+
+  // check if the email has correct format
+  var match = obj.value.match(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/);
+
+  if(match){
+    obj.setCustomValidity("");
+  } else {
+    obj.setCustomValidity("Invalid email. Please check again");
+  }
+}
+
+// validate gender
+function validateGender(form){
+  var male = form["gender1"].checked;
+  var female = form["gender2"].checked;
+  var other = form["gender3"].checked;
+
+  if(male || female || other){
+    form["gender1"].setCustomValidity("");
+  } else {
+    form["gender1"].setCustomValidity("Please select one of these genders");
+  }
+}
+
+// validate and format phone number nicely
+function validatePhoneNumber (form) {
+  var obj = form.elements["phone"];
+
+  var isValid = validateRequiredInput(obj);
+  if(!isValid){
+    return;
+  }
+
+  // filter numbers only
+  var cleaned = ('' + obj.value).replace(/\D/g, '');
   
-  //Check if the input is of correct length
+  // check if the number has correct length (10 digits)
   var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
 
   if (match) {
-    form.elements["phone"].value = '(' + match[1] + ') ' + match[2] + '-' + match[3];
+    obj.value = '(' + match[1] + ') ' + match[2] + '-' + match[3];    // format phone number
+    obj.setCustomValidity("");
+  } else {
+    obj.setCustomValidity("Invalid phone number. Only 10-digit number is allowed");
   }
+}
+
+// validate enquiry
+function validateEnquiry(form){
+  var obj = form.elements["enquiry"];
+  var enquiry = obj.value;
+
+  var isValid = validateRequiredInput(obj);
+  if(!isValid){
+    return;
+  }
+
+  if(enquiry == "General" || enquiry == "Technical" || enquiry == "Events" || enquiry == "Donate"){
+    obj.setCustomValidity("");
+  } else {
+    obj.setCustomValidity("Invalid enquiry. Please check again");
+  }
+}
+
+// validate message
+function validateMessage(form){
+  var obj = form.elements["message"];
+
+  validateRequiredInput(obj);
 }
