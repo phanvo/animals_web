27c588b962b78b067dde47af1da20bb962640055
Phan Vo <phanvo@gmail.com>
2020-07-14 21:25:31 -0700

    add comments; update documentation; refactor code

--- script/scripts_PVo20809.js
+++ script/scripts_PVo20809.js
@@ -3,6 +3,7 @@
     Author: Phan Vo - 300320809
 */
 
+// display/hide links inside hamburger menu
 function hamburgerMenuFunc() {
   var menu = document.getElementById("myLinks");
   if (menu.style.display === "block") {
@@ -12,55 +13,61 @@ function hamburgerMenuFunc() {
   }
 } // end hamburgerMenuFunc
 
-// When the user scrolls down 20px from the top of the document, show the button
+// display/hide the back-to-top button when the user scrolls down from the top of the document, using onsroll event
 window.onscroll = function() {
-  //Get the button:
   var backToTopBtn = document.getElementById("backToTopBtn");
 
-  // only handle back to top button for desktop layout
+  // only handle back-to-top button for desktop layout
   if (screen.width < 769){
     backToTopBtn.style.display = "none";
     return;
   }
 
-  // display back to top button when navigation items are off the screen
+  // display back-to-top button when navigation items are nearly off the screen
   if (document.body.scrollTop > 260 || document.documentElement.scrollTop > 260) {
     backToTopBtn.style.display = "block";
   } else {
     backToTopBtn.style.display = "none";
   }
-};
+}; // end onscroll event
 
-// When the user clicks on the button, scroll to the top of the document
+// scroll back to the top of the document when clicking the button
 function topPageFunc() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0;            // for Safari
+  document.documentElement.scrollTop = 0; // for Chrome, Firefox, IE and Opera
 } // end topPageFunc
 
+// handle onload event of pages
 window.onload = function () { 
-  startClockTime();
-
-  var currentPage = window.location.pathname.split("/").pop();
-  if(currentPage.startsWith("contact")){
-    randomAddressImage();
-  }
-};
-
-function startClockTime() {
-  var today = new Date();
+  startClockTime();   // run clock with date
   
+  var currentPage = window.location.pathname.split("/").pop();    
+  if(currentPage.startsWith("contact")){        // check current page whether it is Contact page
+    randomAddressImage();   // handle random image for the address section of Contact page
+  }
+}; // end onload event
+
+// handle clock with date
+function startClockTime() {
+  var today = new Date();   // get current date
+  
+  // extract year, month, day from current date
   var dateTimeFormat = new Intl.DateTimeFormat('en', { year: 'numeric', month: 'short', day: '2-digit' });
   var [{ value: month },,{ value: day },,{ value: year }] = dateTimeFormat.formatToParts(today);
 
+  // extract hours, minutes, seconds of the current date
   var h = today.getHours();
-  var m = today.getMinutes();
-  var s = today.getSeconds();
-  m = checkTime(m);
-  s = checkTime(s);
+  var m = checkTime(today.getMinutes());
+  var s = checkTime(today.getSeconds());
+
+  // build a full string to display
   document.getElementById('clock').innerHTML = month + ", " + day + ", " + year + " " + h + ":" + m + ":" + s;
+
+  // update current date and time in real time
   setTimeout(startClockTime, 500);
 } // end startClockTime
 
+// format time with leading zero
 function checkTime(i) {
   if (i < 10) {
     i = "0" + i;  // add zero in front of numbers < 10
@@ -68,6 +75,7 @@ function checkTime(i) {
   return i;
 } // end checkTime
 
+// create random image for address section of Contact page
 function randomAddressImage(){
   var folderPath = "images/";
   var fishes = ["cuttlefish.jpg", "killerwhale.jpg", "mandarinfish.jpg", "lionfish.jpg", "piranha.jpg", "tigershark.jpg"];
